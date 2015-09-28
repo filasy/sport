1df87942c0ac7ce8ab0ea1940cd0d646a0e30fbb
jfilasy <jfilasy@gmail.com>
2015-09-28 17:38:32 +0300

    Добавила роль админа при входе на главную страницу

--- grails-app/controllers/sport/ForecastController.groovy
+++ grails-app/controllers/sport/ForecastController.groovy
@@ -5,7 +5,7 @@ import grails.transaction.Transactional
 
 @Transactional(readOnly = true)
 
-@Secured('ROLE_USER')
+@Secured(['ROLE_USER', 'ROLE_ADMIN'])
 class ForecastController {
     def scaffold = Forecast
 }
